eec86c96b687da93c686027062e7f17c5fe4dffd
AdityaNarendra <narendraaditya8@gmail.com>
2025-02-25 16:04:51 +0530

    Changing the location of the files
    We moved the script files to Script folder, and all the required data to Data folder.
    Only the python webapp script is kept in the base folder.
    Additionally, we also deleted unnecessary folders and files.

--- GRB_RSE_webapp-main/Extras/r_scripts/simple_process_data.R
+++
@@ -1,8 +0,0 @@
-# process_data.R
-args <- commandArgs(trailingOnly = TRUE)
-input_file <- args[1]
-output_file <- args[2]
-
-# Example: Read the file and simply write it back out
-data <- read.csv(input_file, header = TRUE)
-write.csv(data, file = output_file, row.names = FALSE)
